d0199f5ab558df495da813033528be5f22d7b1b3
J-A-R-S-H <122942384+J-A-R-S-H@users.noreply.github.com>
2026-02-09 15:02:54 -0800

    finished stickerDisplay

--- class-06-exercise/app/page.tsx
+++ class-06-exercise/app/page.tsx
@@ -1,8 +1,16 @@
 import Image from "next/image";
+import StickerDisplay from "./shared/compononets/StickerDisplay/index"
+import "./styles.css"
 
 export default function Home() {
   return (
-  
+  <div >
+    <main>
   <div>Hello this is my app</div>
+  <StickerDisplay/>
+</main>
+</div>
+
+  
   );
 }
---
+++ class-06-exercise/app/shared/compononets/StickerDisplay/index.js
@@ -0,0 +1,9 @@
+export default function StickerDisplay() {
+  return (
+    <>
+      <div className="flexbox-display">
+        <img src="../../../favicon.ico" alt="favicon" />
+      </div>
+    </>
+  );
+}
