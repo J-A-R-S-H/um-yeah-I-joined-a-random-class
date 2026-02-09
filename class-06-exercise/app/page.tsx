ce29fd25741aafb01a00030887e55d9873c196d9
J-A-R-S-H <122942384+J-A-R-S-H@users.noreply.github.com>
2026-02-09 15:07:13 -0800

    finished sticker gallaery

--- class-06-exercise/app/page.tsx
+++ class-06-exercise/app/page.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
 import StickerDisplay from "./shared/compononets/StickerDisplay/index"
+import StickerGallery from "./shared/compononets/StickerGallery/index"
+
 import "./styles.css"
 
 export default function Home() {
@@ -8,6 +10,7 @@ export default function Home() {
     <main>
   <div>Hello this is my app</div>
   <StickerDisplay/>
+  <StickerGallery />
 </main>
 </div>
 
---
+++ class-06-exercise/app/shared/compononets/StickerGallery/index.js
@@ -0,0 +1,12 @@
+export default function StickerDisplay() {
+  return (
+    <>
+      <div className="flexbox-gallery">
+        <img src="../../../favicon.ico" alt="favicon" />
+        <img src="../../../favicon.ico" alt="favicon" />
+        <img src="../../../favicon.ico" alt="favicon" />
+        <img src="../../../favicon.ico" alt="favicon" />
+      </div>
+    </>
+  );
+}
